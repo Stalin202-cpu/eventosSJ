d9a6e1b149bd2b364be02e153c66f8691482fe2a
polar <stalinjaime@gmail.com>
2025-06-04 22:36:17 -0500

    Se agregar nuevoEstadio2

--- Aplicaciones/Estadios/views.py
+++ Aplicaciones/Estadios/views.py
@@ -1,7 +1,81 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Estadios
+from django.contrib import messages
+from django.conf import settings
+import os
 
-# Create your views here.
 def inicio(request):
     listadoEstadio = Estadios.objects.all()
     return render(request, "inicio.html", {'Estadios': listadoEstadio})
+
+# Renderizar el formulario para nueva prueba
+def nuevoEstadio(request):
+    return render(request, "nuevoEstadio.html")
+
+# Función encargada de guardar una nueva prueba en la base de datos
+def guardarEstadio(request):
+    nombre = request.POST["nombre"]
+    ubicacion = request.POST["ubicacion"]
+    capacidad = request.POST["capacidad"]
+    fecha = request.POST["fecha"]
+
+    #Subiendo archivo con parentecis
+    logo=request.FILES.get("logo")
+    pdf=request.FILES.get("pdf")
+
+    Estadios.objects.create(nombre=nombre, ubicacion=ubicacion, capacidad=capacidad, fecha=fecha, logo=logo, pdf=pdf)
+
+    #Mensaje de confirmacion 
+    messages.success(request, "Cajero guardado exitosamente")
+    return redirect('inicio')
+
+# Eliminar una prueba por ID
+def eliminarCajero(request, id):
+    cajeroElimiar = Cajeros.objects.get(id=id)
+
+    if cajeroElimiar.logo and os.path.isfile(os.path.join(settings.MEDIA_ROOT, cajeroElimiar.logo.name)):
+        os.remove(os.path.join(settings.MEDIA_ROOT, cajeroElimiar.logo.name))
+
+    if cajeroElimiar.pdf and os.path.isfile(os.path.join(settings.MEDIA_ROOT, cajeroElimiar.pdf.name)):
+        os.remove(os.path.join(settings.MEDIA_ROOT, cajeroElimiar.pdf.name))
+
+    cajeroElimiar.delete()
+    return redirect('inicio')
+
+# Mostrando formulario de ediccion
+def editarCajero(request, id):
+    cajeroEditar = Cajeros.objects.get(id=id)
+    return render(request, "editarCajero.html", {'cajeroEditar': cajeroEditar})
+
+def procesarEdicionCajeros(request):
+    
+    id=request.POST["id"]
+    nombre = request.POST["nombre"]
+    cedula = request.POST["cedula"]
+    turno = request.POST["turno"]
+    logo=request.FILES.get("logo")
+    pdf=request.FILES.get("pdf")
+    
+    cajeros2=Cajeros.objects.get(id=id)
+    cajeros2.nombre=nombre
+    cajeros2.cedula=cedula
+    cajeros2.turno=turno
+    if logo :
+        if cajeros2.logo:
+            rutaLogo = os.path.join(settings.MEDIA_ROOT, str(cajeros2.logo))
+            if os.path.isfile(rutaLogo):
+                os.remove(rutaLogo)
+        cajeros2.logo = logo
+
+    if pdf:
+        if cajeros2.pdf:
+            rutaPdf = os.path.join(settings.MEDIA_ROOT, str(cajeros2.pdf))
+            if os.path.isfile(rutaPdf):
+                os.remove(rutaPdf)
+        cajeros2.pdf = pdf
+        
+    cajeros2.save()
+    #Mensaje de confirmacion
+    messages.success(request, "Cajero Actualizado exitosamente")
+    return redirect('inicio')
+
