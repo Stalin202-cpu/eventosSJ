f4047875028bb797a1b940d9f51a882d8c978f15
polar <stalinjaime@gmail.com>
2025-06-04 21:35:07 -0500

    Se agregar Plantilla3

--- Aplicaciones/Estadios/views.py
+++ Aplicaciones/Estadios/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Estadios
 
 # Create your views here.
+def inicio(request):
+    listadoEstadio = Estadios.objects.all()
+    return render(request, "inicio.html", {'Estadios': listadoEstadio})
--- eventosSJ/settings.py
+++ eventosSJ/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'eventosSJ.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'Aplicaciones','template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
