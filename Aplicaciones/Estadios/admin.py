3db2c214982f40be3f2efe175467bb96de99cf83
polar <stalinjaime@gmail.com>
2025-06-04 19:56:06 -0500

    Se agrega models

--- Aplicaciones/Estadios/admin.py
+++ Aplicaciones/Estadios/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Estadios
 
 # Register your models here.
+admin.site.register(Estadios)
--- Aplicaciones/Estadios/models.py
+++ Aplicaciones/Estadios/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Estadios(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    ubicacion = models.CharField(max_length=150)
+    capacidad = models.IntegerField()
+    fecha_inauguracion = models.DateField()
